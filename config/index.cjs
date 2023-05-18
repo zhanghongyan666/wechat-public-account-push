dcb5b9ce0ba7be363dae30867ec5d6e730ffb8f7
zhanghongyan666 <133934850+zhanghongyan666@users.noreply.github.com>
2023-05-18 15:56:16 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'orwxT5w4VrTRe-vSL9PQWBkytrTA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'xa3JWv4dYRcrTepFc0JmLRAYjvhpck-PSHpg3ZDRZgg',
+      useTemplateId: 'Gg_DubaY4mmiZ0J14HKlDnvmmeRsI-7d9HsZ8FejNK4',
       festivals: [
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
