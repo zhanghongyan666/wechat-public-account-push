45e4089b1010824afac2210401a1b45f58882e9f
zhanghongyan666 <133934850+zhanghongyan666@users.noreply.github.com>
2023-05-18 14:37:53 +0800

    11.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,9 +19,9 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '🥕',
+      name: '小张好困',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'orwxT52_tXUJRmT2-cdebmCH0lSY',
+      id: 'orwxT5w4VrTRe-vSL9PQWBkytrTA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'mGp_DzZlyE783WhMSmLW9wBolSoyxFjJVe32P4JEiBc',
       festivals: [
@@ -37,20 +37,5 @@ const USER_CONFIG = {
       ],
     },
   ],
-
-
-  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '63GoyTDcRNQd2fNy9s8MEBICXIx03RQNhMOVLApOeLQ',
-
-  CALLBACK_USERS: [
-    {
-      name: '自己',
-      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'orwxT5w4VrTRe-vSL9PQWBkytrTA',
-    }
-  ],
-
-}
-
 module.exports = USER_CONFIG
 
