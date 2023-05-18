b6f7effa7f5ce96befe32952021636ea80f5bf88
zhanghongyan666 <133934850+zhanghongyan666@users.noreply.github.com>
2023-05-18 12:01:20 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,9 +19,9 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '小张好困',
+      name: '🥕',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'orwxT5w4VrTRe-vSL9PQWBkytrTA',
+      id: 'orwxT52_tXUJRmT2-cdebmCH0lSY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'mGp_DzZlyE783WhMSmLW9wBolSoyxFjJVe32P4JEiBc',
       festivals: [
