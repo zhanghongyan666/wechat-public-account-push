4927399f16bf4d5dac8537218ce6a0b9e224c157
zhanghongyan666 <133934850+zhanghongyan666@users.noreply.github.com>
2023-05-18 11:06:35 +0800

    222.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,13 +19,11 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '乖乖',
+      name: '小张好困',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'orwxT5w4VrTRe-vSL9PQWBkytrTA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'gQMtRCYfRPUElCVshta_-7RY8FnS2PnWS1nj31cqtp8',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
       festivals: [
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
