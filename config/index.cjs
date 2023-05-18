e33479c5acea4c0e708e89fddaae3ffef2fa3f08
zhanghongyan666 <133934850+zhanghongyan666@users.noreply.github.com>
2023-05-18 10:57:33 +0800

    123.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '乖乖',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'orwxT5w4VrTRe-vSL9PQWBkytrTA ',
+      id: 'orwxT5w4VrTRe-vSL9PQWBkytrTA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'gQMtRCYfRPUElCVshta_-7RY8FnS2PnWS1nj31cqtp8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -42,13 +42,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'orwxT5w4VrTRe-vSL9PQWBkytrTA ',
+  CALLBACK_TEMPLATE_ID: 'orwxT5w4VrTRe-vSL9PQWBkytrTA',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'orwxT5w4VrTRe-vSL9PQWBkytrTA ',
+      id: 'orwxT5w4VrTRe-vSL9PQWBkytrTA',
     }
   ],
 
